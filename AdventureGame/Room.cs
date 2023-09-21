5e0ddbb314768e9f9df21831264c8dc7b96c9526
Albert Rakai <albert.rakai@bisimulations.com>
2023-09-21 13:04:09 +0200

    Re-add room class

---
+++ AdventureGame/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventureGame
+{
+	public class Room
+	{
+		public string name;
+
+
+		public void EnterRoom()
+		{
+			Console.WriteLine(" \nYou entered the {0}", name);
+		}
+	}
+}
